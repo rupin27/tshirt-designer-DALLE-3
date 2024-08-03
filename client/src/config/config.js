ea2af4261936edaa5264066d589c2def9e29e699
Rupin Mehra <rupin.mehra@spectrio.com>
2024-08-03 14:54:55 -0400

    Update: Adding render backend

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,9 +1,9 @@
 const config = {
   development: {
-    backendUrl: "http://localhost:8080/api/v1/dalle",
+    backendUrl: "https://tshirt-designer-dalle-3.onrender.com/api/v1/dalle",
   },
   production: {
-    backendUrl: "https://devswag.onrender.com/api/v1/dalle",
+    backendUrl: "https://tshirt-designer-dalle-3.onrender.com/api/v1/dalle",
   },
 };
 
